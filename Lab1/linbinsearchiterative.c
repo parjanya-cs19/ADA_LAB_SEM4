2ec3f6641be5a30684f13bfcbacbe3f844db020b
Parjanya Modi <parjanya.cs19@bmsce.ac.in>
2021-04-20 14:38:55 +0530

    Updates

--- Lab1/linbinsearchiterative.c
+++ Lab1/linbinsearchiterative.c
@@ -1,14 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int linearSearch(int array[], int size, int val) 
-{ 
-    for (int i = 0; i < size; i++) 
-        if (array[i] == val) 
-            return i; 
-    return -1; 
-} 
-
+int linearSearch(int array[], int size, int val)
+{
+    for (int i = 0; i < size; i++)
+        if (array[i] == val)
+            return i;
+    return -1;
+}
 
 int binarySearch(int arry[], int val, int high)
 {
@@ -25,75 +24,76 @@ int binarySearch(int arry[], int val, int high)
             }
         }
     }
-    for(i=0; i< high; i++)
+    for (i = 0; i < high; i++)
     {
-        printf("%4d\n",arry[i]);
+        printf("%4d\n", arry[i]);
     }
-    int mid,flag=0,low=0;
-    while(low<=high&& flag==0)
+    int mid, flag = 0, low = 0;
+    while (low <= high && flag == 0)
     {
-        mid=(low+high)/2;
-        if(arry[mid]==val)
+        mid = (low + high) / 2;
+        if (arry[mid] == val)
         {
-            flag=1;
+            flag = 1;
         }
-        if(val>arry[mid])
+        if (val > arry[mid])
         {
-            low=mid+1;
+            low = mid + 1;
         }
         else
         {
-            high=mid-1;
+            high = mid - 1;
         }
     }
-    if(flag==0)
+    if (flag == 0)
     {
         printf("Element not found!\n");
     }
     else
     {
-        printf("Key found at position %d\n",mid+1);
+        printf("Key found at position %d\n", mid + 1);
     }
 }
 
- 
-int main() 
+int main()
 {
-    int n=0, choice, key;
+    int n = 0, choice, key;
     printf("\nEnter the size of Array : ");
-    scanf("%d",&n);
-    int arr[n];
-    for(int j = 0; j < n; j++)
-          {
-                  arr[j] = (rand() % 200)+1;
-                  printf("%4d\n", arr[j]);
-       }
-    for(;;)
+    scanf("%d", &n);
+    int arr[n],arrl[n],arrb[n];
+    for (int j = 0; j < n; j++)
+    {
+        arr[j] = (rand() % 200) + 1;
+        printf("%4d\n", arr[j]);
+    }
+    arrl=arr;
+    arrb=arr;
+    for (;;)
     {
         printf("\nMenu\n1. Linear Search\n2. Binary Search\n3.Exit\nEnter the choice from menu : ");
-        scanf("%d",&choice);
+        scanf("%d", &choice);
         switch (choice)
         {
-            case 1:
-                printf("\nEnter the element to be searched : ");
-                scanf("%d",&key);
-                int linres = linearSearch(arr, n, key);
-                if (linres == -1)
-                    printf("Element not found!!");
-                else
-                    printf("Item is present at index %d", linres);
-                break;
-            case 2:
-                printf("\nEnter the element to be searched : ");
-                scanf("%d",&key);
-                binarySearch(arr, key,n);
-                
-                break;
-            case 3:
-                exit(0);
-            default:
-                printf("Invalid Choice !");
-                break;
+        case 1:
+            printf("\nEnter the element to be searched : ");
+            scanf("%d", &key);
+            int linres = linearSearch(arr, n, key);
+            if (linres == -1)
+                printf("Element not found!!");
+            else
+                printf("Item is present at index %d", linres);
+            break;
+        case 2:
+            printf("\nEnter the element to be searched : ");
+            scanf("%d", &key);
+            binarySearch(arr, key, n);
+
+            break;
+        case 3:
+            exit(0);
+        default:
+            printf("Invalid Choice !");
+            break;
         }
     }
-} 
+}
--- Lab2/linbinsearchrecursive.c
+++ Lab2/linbinsearchrecursive.c
@@ -1,1 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int linearSearch(int array[], int size, int key)
+{
+    int location;
+    if (array[size] == key)
+    {
+        return size;
+    }
+    else if (size == -1)
+    {
+        return -1;
+    }
+    else
+    {
+        return (location = linearSearch(array, size - 1, key));
+    }
+}
+
+void bubble_sort(int arrb[], int size)
+{
+    int temp, i, j;
+    for (i = 0; i < size; i++)
+    {
+        for (j = i; j < size; j++)
+        {
+            if (arrb[i] > arrb[j])
+            {
+                temp = arrb[i];
+                arrb[i] = arrb[j];
+                arrb[j] = temp;
+            }
+        }
+    }
+    for (int j = 0; j < size; j++)
+    {
+        printf("%4d\n", arrb[j]);
+    }
+}
+
+int binarySearch(int arrb[],int lo, int key, int hi)
+{
+    int mid;
+ 
+    if (lo > hi)
+    {
+        printf("Key not found\n");
+        return;
+    }
+    mid = (lo + hi) / 2;
+    if (arrb[mid] == key)
+    {
+        printf("Key found\n");
+    }
+    else if (arrb[mid] > key)
+    {
+        binarySearch(arrb, lo, mid - 1, key);
+    }
+    else if (arrb[mid] < key)
+    {
+        binarySearch(arrb, mid + 1, hi, key);
+    }
+}
+
+int main()
+{
+    int n = 0, choice, key;
+    printf("\nEnter the size of Array : ");
+    scanf("%d", &n);
+    int arr[n], arrl[n], arrb[n];
+    for (int j = 0; j < n; j++)
+    {
+        arr[j] = (rand() % 200) + 1;
+        printf("%4d\n", arr[j]);
+    }
+    for(int i=0; i<n; i++)
+    {
+        arrl[i]=arr[i];
+        arrb[i]=arr[i];
+    }
+    for (;;)
+    {
+        printf("\nMenu\n1. Linear Search\n2. Binary Search\n3.Exit\nEnter the choice from menu : ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("\nEnter the element to be searched : ");
+            scanf("%d", &key);
+            int linres = linearSearch(arrl, n, key);
+            if (linres == -1)
+                printf("Element not found!!");
+            else
+                printf("Item is present at index %d", linres);
+            break;
+        case 2:
+            printf("\nEnter the element to be searched : ");
+            scanf("%d", &key);
+            bubble_sort(arrb, n);
+            binarySearch(arrb,0, key, n);
+
+            break;
+        case 3:
+            exit(0);
+        default:
+            printf("Invalid Choice !");
+            break;
+        }
+    }
+}
